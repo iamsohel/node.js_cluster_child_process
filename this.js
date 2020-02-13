b668caba2260398ebd9429c454e2a5cb1e857ceb
sohel <sohel.rana@ergo-ventures.com>
2020-02-13 19:11:35 +0600

    this key word

--- this.js
+++ this.js
@@ -15,3 +15,19 @@ console.log(person.full_name());
 const full_name = person.full_name.bind(person);
 console.log(full_name);
 console.log(full_name())
+
+//arrow function don't rebind this keyword
+
+//in arrow function this is inherit it's context where it's code is defined
+
+const man = {
+    walk() {
+        console.log(this) // it will refer man object;
+
+        setTimeout(function () {
+            console.log(this); // it will refer global object cause setTime out er moddhe func ta anonymous function. eta stand alone hisabe kaj korbe
+        }, 1000) // so ami jodi arrow func use kori taile o ei function ke standalone hote dibe na. current man object refer dhoere  eta ke prit korbe
+    }
+}
+
+man.walk();
